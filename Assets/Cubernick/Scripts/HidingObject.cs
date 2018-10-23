1ffeb3bc8a3dc12b6b5786d40c432b412dcc4382
BosonHBC <a923000213@163.com>
2018-10-23 15:57:09 -0600

    Flash Light Implemented

---
+++ Assets/Cubernick/Scripts/HidingObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidingObject : MonoBehaviour {
+
+    public bool bHiding;
+    private Transform tr_Meshes;
+	// Use this for initialization
+	void Start () {
+        tr_Meshes = transform.GetChild(0);
+        tr_Meshes.gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnAndOff(bool _bOn)
+    {
+        bHiding = !_bOn;
+        if (bHiding)
+            tr_Meshes.gameObject.SetActive(false);
+        else
+            tr_Meshes.gameObject.SetActive(true);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("FlashLight"))
+        {
+            OnAndOff(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("FlashLight"))
+        {
+            OnAndOff(false);
+        }
+    }
+}
---
+++ Assets/Cubernick/Scripts/LightPoionter.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPoionter : MonoBehaviour {
+    [SerializeField]  private float f_maxDetectDist;
+    [SerializeField] private float f_maxRadius;
+    private float f_currDetectDist  =0;
+    private  SphereCollider sc_detector;
+    [SerializeField] LayerMask mask;
+
+    [SerializeField] private bool b_lightOn;
+	// Use this for initialization
+	void Start () {
+        sc_detector = GetComponent<SphereCollider>();
+        TurnOnOffLight(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        DebugDraw();
+        if(b_lightOn)
+            RayCastToShowObject();
+    }
+
+    public void TurnOnOffLight(bool _bON)
+    {
+        b_lightOn = _bON;
+        if (b_lightOn)
+        {
+            // turn on light
+            transform.GetChild(0).gameObject.SetActive(true);
+            sc_detector.enabled = true;
+        }
+        else
+        {
+            // turn off light
+            transform.GetChild(0).gameObject.SetActive(false);
+            sc_detector.enabled = false;
+        }
+    }
+
+    GameObject RayCastToShowObject()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray,out hit, f_maxDetectDist, mask))
+        {
+            f_currDetectDist = Vector3.Distance(hit.point, transform.position);
+            sc_detector.radius = f_maxRadius * f_currDetectDist / f_maxDetectDist;
+            sc_detector.center = new Vector3(sc_detector.center.x, sc_detector.center.y, f_currDetectDist);
+            return hit.transform.gameObject;
+        }
+        else
+        {
+            f_currDetectDist = 0;
+            sc_detector.center = new Vector3(sc_detector.center.x, sc_detector.center.y, f_maxDetectDist);
+            sc_detector.radius = f_maxRadius;
+        }
+
+        return null;
+    }
+    void DebugDraw()
+    {
+        Debug.DrawLine(transform.position, transform.position + transform.forward * f_currDetectDist, Color.red);
+    }
+}
