10a3e9a259edabc2b1860f47d81ec062803ae3ff
BosonHBC <a923000213@163.com>
2018-10-29 13:53:46 -0600

    Cut Body, Axe, 6 part spring

---
+++ Assets/Cubernick/Scripts/Body/BodyController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyController : MonoBehaviour
+{
+
+
+    List<Rigidbody> rb_bodyParts = new List<Rigidbody>();
+    List<SpringJoint> sj_bodyConnects = new List<SpringJoint>();
+
+    // if false, set isKinematic is false
+    [SerializeField] bool b_AffectByPhysic;
+    // Use this for initialization
+    void Start()
+    {
+
+        // init list
+        InitBodyParts();
+
+        SetPhysic(b_AffectByPhysic);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetPhysic(bool _Affect)
+    {
+        foreach (var item in rb_bodyParts)
+        {
+            item.isKinematic = !_Affect;
+            item.useGravity = _Affect;
+        }
+
+    }
+    public void InitBodyParts()
+    {
+        sj_bodyConnects = new List<SpringJoint>();
+        rb_bodyParts = new List<Rigidbody>();
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).GetComponent<SpringJoint>() != null)
+            {
+                // sj depends on rb, so there should be rb when there is sj
+                rb_bodyParts.Add(transform.GetChild(i).GetComponent<Rigidbody>());
+                sj_bodyConnects.Add(transform.GetChild(i).GetComponent<SpringJoint>());
+                transform.GetChild(i).GetComponent<CutBody>().i_ID = i;
+            }
+        }
+    }
+}
---
+++ Assets/Cubernick/Scripts/Body/CutBody.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutBody : MonoBehaviour {
+
+    public int i_ID;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BreakThisBodyPart()
+    {
+        BodyController _tempCtrl = transform.parent.GetComponent<BodyController>();
+        // go to world space
+        transform.parent = null;
+        _tempCtrl.InitBodyParts();
+
+        OnOffGrabFunction(true);
+    }
+
+    void OnOffGrabFunction(bool _OnOrOff)
+    {
+        transform.GetComponent<HoloToolkit.Unity.InputModule.Examples.Grabbables.ThrowableObject>().enabled = _OnOrOff;
+        transform.GetComponent<HoloToolkit.Unity.InputModule.Examples.Grabbables.GrabbableChild>().enabled = _OnOrOff;
+        Rigidbody rb = transform.GetComponent<Rigidbody>();
+        Destroy(transform.GetComponent<SpringJoint>());
+        rb.isKinematic = false;
+        rb.useGravity = true;
+    }
+}
