dd55017a683070b2ab51ef45c40506cb14d44df6
BosonHBC <a923000213@163.com>
2018-10-29 18:28:19 -0600

    Drawer Not Done yet

---
+++ Assets/Cubernick/Scripts/Drawer/Drawer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : MonoBehaviour {
+
+    [SerializeField] GameObject objInDrawer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("DrawerDest"))
+        {
+            objInDrawer.SetActive(true);
+            objInDrawer.transform.parent = null;
+        }
+    }
+}
